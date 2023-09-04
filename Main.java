adfa8c771e61ff070bf3f74947f32ce42dac91d9
siddltkfkd <unji2000@gmail.com>
2023-09-04 19:17:16 +0900

    정사각형 넓이 구하기

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Plus plus = new Plus();
+        Area area = new Area();
+
+        System.out.println(area.area(5));
         
         System.out.println(plus.plus(1, 1));
     }
